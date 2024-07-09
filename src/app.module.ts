708ba2bae617c98ce2e5c584c620cfc82d7022da
rizalafaandi <afrizalafaandi@gmail.com>
2024-07-09 14:11:41 +0700

    feat: static route media

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,19 @@ import { LearnModule } from './learn/learn.module';
 import { TaskModule } from './task/task.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [LearnModule, TaskModule, PrismaModule, AuthModule],
+  imports: [
+    LearnModule,
+    TaskModule,
+    PrismaModule,
+    AuthModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
